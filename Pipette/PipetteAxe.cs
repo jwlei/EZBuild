cac49341cd6520d68af6201a5d2ddc7c2776b8d0
jwlei <joleiros@gmail.com>
2022-10-29 22:56:18 +0200

    Added: Ability to prefer 2h or 1h axe for treees pipetting

--- Pipette/PipetteAxe.cs
+++ Pipette/PipetteAxe.cs
@@ -1,5 +1,4 @@
-﻿namespace EZBuild
-{
+﻿namespace EZBuild {
 
     using System;
     using System.Collections.Generic;
@@ -31,11 +30,13 @@
                 }
  
 
-                Predicate<ItemDrop.ItemData> isAxe = delegate (ItemDrop.ItemData item)
-                    {return item.m_shared.m_name.Contains("$item_axe");};
+                Predicate<ItemDrop.ItemData> isAxe = delegate (ItemDrop.ItemData item) {
+                    return item.m_shared.m_name.Contains("$item_axe");
+                };
 
-                Predicate<ItemDrop.ItemData> isBattleaxe = delegate (ItemDrop.ItemData item)
-                    {return item.m_shared.m_name.Contains("$item_battleaxe");};
+                Predicate<ItemDrop.ItemData> isBattleaxe = delegate (ItemDrop.ItemData item) {
+                    return item.m_shared.m_name.Contains("$item_battleaxe");
+                };
 
 
                 List<ItemDrop.ItemData> axes = player.m_inventory.m_inventory.FindAll(isAxe);
@@ -45,34 +46,69 @@
                 List<ItemDrop.ItemData> durableBattleaxe = battleaxes.Where(battleaxe => battleaxe.m_durability != 0).ToList();
 
 
-                if (durableAxes.Count > 0) {
+                // Find 2H axe first if true, 1H first if false
+                if (EZBuild.Prefer_2H_Axe.Value) {
+                    // 2H PRIO --------------------------------------------------
+                    if (durableBattleaxe.Count > 0) {
 
-                    int maxTier = durableAxes.Max(axe => axe.m_shared.m_toolTier);
-                    List<ItemDrop.ItemData> topTierAxes = durableAxes.Where(axe => axe.m_shared.m_toolTier == maxTier).ToList();
-                    topTierAxes.Sort(new CompareDurability());
+                        int maxTier = durableBattleaxe.Max(Battleaxe => Battleaxe.m_shared.m_toolTier);
+                        List<ItemDrop.ItemData> topTierBattleaxes = durableBattleaxe.Where(Battleaxe => Battleaxe.m_shared.m_toolTier == maxTier).ToList();
+                        topTierBattleaxes.Sort(new CompareDurability());
 
-                    if (topTierAxes.Count > 0) {
-                        player.QueueUnequipItem(player.m_rightItem);
-                        player.QueueEquipItem(topTierAxes[0]);
+                        if (topTierBattleaxes.Count > 0) {
+                            player.QueueUnequipItem(player.m_rightItem);
+                            player.QueueEquipItem(topTierBattleaxes[0]);
 
-                        return false;
-                    }
+                            return false;
+                        }
 
-                } else if (durableBattleaxe.Count > 0) {
+                    } else if (durableAxes.Count > 0) {
 
-                    int maxTier = durableBattleaxe.Max(Battleaxe => Battleaxe.m_shared.m_toolTier);
-                    List<ItemDrop.ItemData> topTierBattleaxes = durableBattleaxe.Where(Battleaxe => Battleaxe.m_shared.m_toolTier == maxTier).ToList();
-                    topTierBattleaxes.Sort(new CompareDurability());
-                    
-                    if (topTierBattleaxes.Count > 0) {
-                        player.QueueUnequipItem(player.m_rightItem);
-                        player.QueueEquipItem(topTierBattleaxes[0]);
+                        int maxTier = durableAxes.Max(axe => axe.m_shared.m_toolTier);
+                        List<ItemDrop.ItemData> topTierAxes = durableAxes.Where(axe => axe.m_shared.m_toolTier == maxTier).ToList();
+                        topTierAxes.Sort(new CompareDurability());
 
-                        return false;
+                        if (topTierAxes.Count > 0) {
+                            player.QueueUnequipItem(player.m_rightItem);
+                            player.QueueEquipItem(topTierAxes[0]);
+
+                            return false;
+                        }
                     }
                 }
-            }
-            return true;
+                
+
+                
+                if (!EZBuild.Prefer_2H_Axe.Value) {
+                    // 1H PRIO --------------------------------------------------
+                    if (durableAxes.Count > 0) {
+
+                        int maxTier = durableAxes.Max(axe => axe.m_shared.m_toolTier);
+                        List<ItemDrop.ItemData> topTierAxes = durableAxes.Where(axe => axe.m_shared.m_toolTier == maxTier).ToList();
+                        topTierAxes.Sort(new CompareDurability());
+
+                        if (topTierAxes.Count > 0) {
+                            player.QueueUnequipItem(player.m_rightItem);
+                            player.QueueEquipItem(topTierAxes[0]);
+
+                            return false;
+                        }
+
+                    } else if (durableBattleaxe.Count > 0) {
+
+                        int maxTier = durableBattleaxe.Max(Battleaxe => Battleaxe.m_shared.m_toolTier);
+                        List<ItemDrop.ItemData> topTierBattleaxes = durableBattleaxe.Where(Battleaxe => Battleaxe.m_shared.m_toolTier == maxTier).ToList();
+                        topTierBattleaxes.Sort(new CompareDurability());
+
+                        if (topTierBattleaxes.Count > 0) {
+                            player.QueueUnequipItem(player.m_rightItem);
+                            player.QueueEquipItem(topTierBattleaxes[0]);
+
+                            return false;
+                        }
+                    }
+                }
+            } return true;
         }
     }
 }
--- Util/Config.cs
+++ Util/Config.cs
@@ -20,50 +20,63 @@
 			get; 
 			set;
 		}
-		public static ConfigEntry<bool> Pipette
-		{
+
+		public static ConfigEntry<bool> Prefer_2H_Axe {
 			get;
 			set;
 		}
+
 		public static ConfigEntry<KeyboardShortcut> Pipette_Hotkey {
 			get;
 			set;
 		}
 
-		public static ConfigEntry<bool> Repair
-		{
-			get;
-			set;
-		}
 		public static ConfigEntry<KeyboardShortcut> Repair_Hotkey { 
 			get; 
 			set; 
 		}
-		public static ConfigEntry<bool> Hammer
-		{
-			get;
-			set;
-		}
+	
 		public static ConfigEntry<KeyboardShortcut> Hammer_Hotkey { 
 			get; 
 			set;
 		}
 
 
-        private void LoadConfig() {
+
+
+		private void LoadConfig() {
 			// Config menu in order
 
-			// 1
-			EnableMod = base.Config.Bind<bool>(GENERAL_SETTINGS, "Enable EZ Build", true, "Enables or disables the mod");
 
-			InteractWithToolEquipped = base.Config.Bind<bool>(GENERAL_SETTINGS, "Enable interaction when a tool is equipped", true, "Enables interaction with e.g. chests, ..., workbenches while holding tools");
+			EnableMod = base.Config.Bind<bool>(GENERAL_SETTINGS, 
+												"1. Enable EZ Build", 
+												true, 
+												"Enables or disables the mod");
 
-			Pipette_Hotkey = base.Config.Bind<KeyboardShortcut>(GENERAL_SETTINGS, "Hotkey for pipette", new KeyboardShortcut(KeyCode.Q), "Pipette tool to copy piece and orientation of the build piece you're pointing at, or equipping the correct tool for the item you're aimed at.");
+			InteractWithToolEquipped = base.Config.Bind<bool>(GENERAL_SETTINGS, 
+												"2. Enable interaction when a tool is equipped", 
+												true, 
+												"Enables interaction with e.g. chests, ..., workbenches while holding tools");
 
-			Repair_Hotkey = base.Config.Bind<KeyboardShortcut>(GENERAL_SETTINGS, "Hotkey for repairing", new KeyboardShortcut(KeyCode.V), "With the hammer equipped, press the hotkey to repair the item you are pointing at.");
+			Prefer_2H_Axe = base.Config.Bind<bool>(GENERAL_SETTINGS, 
+												"3. Prefer 2H Axe over 1H Axe", 
+												false, 
+												"Toggles which axe is preffered when pipetting on trees etc.");
 
-			Hammer_Hotkey = base.Config.Bind<KeyboardShortcut>(GENERAL_SETTINGS, "Hotkey for equipping hammer", new KeyboardShortcut(KeyCode.B), "Hotkey for equipping Hammer");
+			Pipette_Hotkey = base.Config.Bind<KeyboardShortcut>(GENERAL_SETTINGS, 
+												"4. Hotkey for pipette", 
+												new KeyboardShortcut(KeyCode.Q), 
+												"Pipette tool to copy piece and orientation of the build piece you're pointing at, or equipping the correct tool for the item you're aimed at.");
 
+			Repair_Hotkey = base.Config.Bind<KeyboardShortcut>(GENERAL_SETTINGS, 
+												"5. Hotkey for repairing", 
+												new KeyboardShortcut(KeyCode.V), 
+												"With the hammer equipped, press the hotkey to repair the item you are pointing at.");
+
+			Hammer_Hotkey = base.Config.Bind<KeyboardShortcut>(GENERAL_SETTINGS, 
+												"6. Hotkey for equipping hammer", 
+												new KeyboardShortcut(KeyCode.B), 
+												"Hotkey for equipping Hammer");
 		}
 	}
 }
